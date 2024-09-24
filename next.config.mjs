7fdb9b454004f6f8ad5bf163b99b5ae2ca9047fb
vikaswakde <vikaswakdepc@gmail.com>
2024-09-24 19:36:26 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'assets.example.com',
+            port: '',
+            pathname: '/account123/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'nextjs.org',
+            port: '',
+            pathname: '/icons/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'github.githubassets.com',
+            port: '',
+            pathname: '/assets/**',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
