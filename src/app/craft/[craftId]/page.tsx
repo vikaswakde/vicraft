74933c2041643bfc142d79cf68a8b321dcbec0c1
vikaswakde <vikaswakdepc@gmail.com>
2024-11-29 11:09:25 +0530

    'change bg color'

--- src/app/craft/[craftId]/page.tsx
+++ src/app/craft/[craftId]/page.tsx
@@ -1,33 +1,33 @@
-'use client'
+"use client";
 
-import { useParams } from 'next/navigation'
-import FeedbackButton from '@/crafts/FeedbackButton'
-import GooeyMenu from '@/crafts/GooeyMenu'
-import RadialMenu from '@/crafts/RadialMenu'
-import AirportAirPlaneTakeOff from '@/crafts/AirportAirPlaneTakeOff'
+import { useParams } from "next/navigation";
+import FeedbackButton from "@/crafts/FeedbackButton";
+import GooeyMenu from "@/crafts/GooeyMenu";
+import RadialMenu from "@/crafts/RadialMenu";
+import AirportAirPlaneTakeOff from "@/crafts/AirportAirPlaneTakeOff";
 // Import other craft components as needed
 
 const crafts = {
-  'feedback-button': FeedbackButton,
-  'gooey-menu': GooeyMenu,
-  'radial-menu': RadialMenu,
-  'airport-airplane-take-off': AirportAirPlaneTakeOff,
+  "feedback-button": FeedbackButton,
+  "gooey-menu": GooeyMenu,
+  "radial-menu": RadialMenu,
+  "airport-airplane-take-off": AirportAirPlaneTakeOff,
   // Add other crafts here
-}
+};
 
 export default function CraftPage() {
-  const params = useParams()
-  const craftId = params.craftId as string
+  const params = useParams();
+  const craftId = params.craftId as string;
 
-  const CraftComponent = crafts[craftId as keyof typeof crafts]
+  const CraftComponent = crafts[craftId as keyof typeof crafts];
 
   if (!CraftComponent) {
-    return <div>Craft not found</div>
+    return <div>Craft not found</div>;
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen bg-teal-200">
       <CraftComponent />
     </div>
-  )
+  );
 }
